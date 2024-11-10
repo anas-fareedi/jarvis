dfd1846937b4011a117791c54f3daca56afa0c79
Anas Fareedi <anasfareedi786786@gmail.com>
2024-11-10 11:33:25 +0530

    added conition to win a game

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,32 +1,18 @@
 import random 
-computer = random.choice([-1,0,1])
-yourchoice = input("enter your choice -> Rock , Paper , Scissor\n")
+computer = random.choice([-1,0,1]) 
+yourchoice = input("enter your choice -> Rock , Paper , Scissor\n") 
 
-Dict ={"rock":-1,"paper":0,"scissor":1}
-revDict={-1:"rock",0:"paper",1:"scissor"}
-you = Dict[yourchoice]
+Dict ={"rock":-1,"paper":0,"scissor":1} 
+revDict={-1:"rock",0:"paper",1:"scissor"} 
+you = Dict[yourchoice]  
 
-print(f"you choose {revDict[you]} computer choose {revDict[computer]}")
+print(f"you choose {revDict[you]} computer choose {revDict[computer]}") 
 
-if computer == you:
-    print("match Draw !")
+if computer == you: 
+    print("match Draw !") 
     
-else:
-    if you == -1 and computer == 0:
-        print("computer wins")
-    elif you == -1 and computer == 1:
-        print("you wins !")
-    
-    elif you == 0 and computer == -1:
-        print("computer wins !")
-    elif you == 0 and computer == 1:
-        print("you wins !")
-        
-    elif you == 1 and computer == -1:
-        print("computer wins !")
-    elif you == 1 and computer == 0:
-        print("you wins !")
-        
+else: 
+    if (computer - you) == -1 or (computer - you) == 2 :
+        print("you win !")
     else:
-        print("something went wrong")
-    
+        print("computer wins !")
